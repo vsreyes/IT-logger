ccb38865faba7a5d557871a29de913c1f5203dcb
Virgilio Sardido Reyes <virgilio.s.reyes@gmail.com>
2020-07-28 22:24:04 +0200

    Added tech delete and add

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import TechSelectOptions from '../techs/TechSelectOptions';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addLog } from '../../actions/logActions';
@@ -60,12 +61,11 @@ const AddLogModal = ({ addLog }) => {
               <option value='' disabled>
                 Selection Technician
               </option>
-              <option value='John Doe'>John Doe</option>
-              <option value='Sam Smith'>Sam Smith</option>
-              <option value='Sara Wilson'>Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
+
         <div className='row'>
           <div className='input-field'>
             <p>
